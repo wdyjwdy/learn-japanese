126a84bc492cc3672af98152605e6c166b51949a
wdyjwdy <xxccat@gmail.com>
2025-04-19 12:46:29 +0800

    init

---
+++ app/layout.jsx
@@ -0,0 +1,51 @@
+import { Layout, Navbar } from "nextra-theme-docs";
+import { Head, Search, Image } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
+import "./styles.css";
+
+export const metadata = {
+  // Define your metadata here
+  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+};
+
+const navbar = (
+  <Navbar
+    logo={<Image width="24" height="24" src="/logo.png" />}
+    // ... Your additional navbar options
+  />
+);
+
+export default async function RootLayout({ children }) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body>
+        <Layout
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          editLink={null}
+          feedback={{ content: null }}
+          navigation={false}
+          sidebar={{ toggleButton: false }}
+          toc={{ backToTop: false }}
+          darkMode={false}
+          search={<Search placeholder="Search" />}
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  );
+}
---
+++ content/_meta.js
@@ -0,0 +1,21 @@
+import { Layout } from "nextra-theme-docs";
+
+export default {
+  index: {
+    title: "Home",
+    type: "page",
+    display: "hidden",
+    theme: {
+      layout: "full",
+      timestamp: false,
+    },
+  },
+  grammar: {
+    title: "语法",
+    type: "page",
+  },
+  vocab: {
+    title: "词汇",
+    type: "page",
+  },
+};
