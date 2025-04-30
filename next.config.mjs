6a27aef92b1a5740f4fa1816964cf43b2ae3fb4c
wdyjwdy <xxccat@gmail.com>
2025-05-01 05:43:39 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import nextra from "nextra";
 
 const withNextra = nextra({
   // ... Other Nextra config options
+  search: { codeblocks: false },
 });
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
